bd999cf7011bacb08ba0671b6de1e38d1840427c
Gerald Agyapong <geraldagyapong2018@gmail.com>
2025-07-03 13:19:48 +0100

    Add Docker deployment files

--- QuizMeWeb/tailwind.config.js
+++ QuizMeWeb/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}", // Important to include Angular files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
